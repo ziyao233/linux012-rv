693b70d5d335c6d155bc2ba2f498ebdcd03cbe98
Ziyao <ziyao@disroot.org>
2023-05-07 10:35:53 +0800

    Enable paging while booting

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -29,9 +29,9 @@ extern inline void oom(void)
 __asm__("movl %%eax,%%cr3"::"a" (0))
 
 /* these are not to be changed without changing head.s etc */
-#define LOW_MEM 0x80100000
+#define LOW_MEM 0x80200000
 extern unsigned long HIGH_MEMORY;
-#define PAGING_MEMORY (15 * 1024 * 1024)
+#define PAGING_MEMORY (16 * 1024 * 1024)
 #define PAGING_PAGES (PAGING_MEMORY>>12)
 #define MAP_NR(addr) (((addr)-LOW_MEM)>>12)
 #define USED 100
